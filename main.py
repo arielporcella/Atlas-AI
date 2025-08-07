a6259fd091162388fb23a84fe0534337fd8f6404
arielporcella <adreamsagency@gmail.com>
2025-08-07 14:34:34 -0300

    main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 import requests
 from bs4 import BeautifulSoup
 
-app = Flask(__name__)
+app = Flask(atlasai)
 
 def scrape_site(url, selector="a", base_url=None, limit=5):
     try:
